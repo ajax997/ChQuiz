bd7f1311158727371435afefd926c011cee54589
nguyennghi <nguyennghi1997@outlook.com>
2026-07-29 01:06:40 +0700

    Update mongo_connect.py

--- mongo_connect.py
+++ mongo_connect.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-def test_mongo_connection:
+def test_mongo_connection():
     uri = (
         f"mongodb+srv://nnguyen231dxc_db_user:E0JR83lvL8NlsqWy@cluster0.i7cibfp.mongodb.net/?appName=Cluster0"
     )
