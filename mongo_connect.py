83ebed648e08ed8f5ec66f9e8ce46791f4539d12
nguyennghi <nguyennghi1997@outlook.com>
2026-07-29 01:01:41 +0700

    Update mongo_connect.py

--- mongo_connect.py
+++ mongo_connect.py
@@ -1,16 +1,14 @@
 from pymongo import MongoClient
-
-username = "nnguyen231dxc_db_user"
-password = ""
-
-uri = (
-    f"mongodb+srv://nnguyen231dxc_db_user:E0JR83lvL8NlsqWy@cluster0.i7cibfp.mongodb.net/?appName=Cluster0"
-)
-
-client = MongoClient(uri)
-
-db = client["ChQuiz"]
-collection = db["QuizDataSet"]
-
-for doc in collection.find():
-    print(doc)
+def test_mongo_connection:
+    uri = (
+        f"mongodb+srv://nnguyen231dxc_db_user:E0JR83lvL8NlsqWy@cluster0.i7cibfp.mongodb.net/?appName=Cluster0"
+    )
+    
+    client = MongoClient(uri)
+    
+    db = client["ChQuiz"]
+    collection = db["QuizDataSet"]
+    a = []
+    for doc in collection.find():
+        a.append(str(doc))
+    return a
